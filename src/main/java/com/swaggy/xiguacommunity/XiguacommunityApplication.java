7c7ca8289511e1f25628e05bf49b9267ad1dc568
SwaggyPPP <937006028@qq.com>
2020-04-28 10:49:20 +0800

    init repo

---
+++ src/main/java/com/swaggy/xiguacommunity/XiguacommunityApplication.java
@@ -0,0 +1,13 @@
+package com.swaggy.xiguacommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiguacommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiguacommunityApplication.class, args);
+    }
+
+}
